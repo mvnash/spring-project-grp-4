08cab2192266753fcffde80120fa92d7e0531842
louisvanbel <louis.vanbel@student.vinci.be>
2023-11-16 14:07:01 +0100

    feat: add order repository

---
+++ order/src/main/java/be/vinci/ipl/order/OrderRepository.java
@@ -0,0 +1,31 @@
+package be.vinci.ipl.order;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Repository interface for managing orders in the system.
+ */
+@Repository
+public interface OrderRepository extends CrudRepository<Order, String> {
+
+  /**
+   * Retrieves all orders placed by a specific owner.
+   *
+   * @param owner The username of the owner.
+   * @return List of orders placed by the specified owner.
+   */
+  List<Order> findByOwner(String owner);
+
+  /**
+   * Retrieves all open orders for a specific ticker and side.
+   *
+   * @param ticker The ticker symbol of the instrument.
+   * @param side   The side of the order (BUY or SELL).
+   * @return List of open orders for the specified ticker and side.
+   */
+  List<Order> findOpenOrdersByTickerAndSide(String ticker, OrderSide side);
+
+}
