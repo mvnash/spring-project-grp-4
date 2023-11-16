e88f20534a21304537e008bafba262f30fd608a2
louisvanbel <louis.vanbel@student.vinci.be>
2023-11-16 14:13:36 +0100

    feat: add dependencies

--- order/src/main/java/be/vinci/ipl/order/OrderApplication.java
+++ order/src/main/java/be/vinci/ipl/order/OrderApplication.java
@@ -2,10 +2,30 @@ package be.vinci.ipl.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * The main class for the Order microservice application.
+ *
+ * <p>This class is annotated with {@code @SpringBootApplication} to enable Spring Boot features and
+ * {@code @EnableDiscoveryClient} for service discovery in a microservices environment.
+ * It includes the main method to run the Spring Boot application.</p>
+ *
+ * <p>The application utilizes the {@code @EnableDiscoveryClient} annotation to register itself
+ * with the service discovery component, allowing other services to discover and communicate with it.</p>
+ *
+ * @see SpringBootApplication
+ * @see EnableDiscoveryClient
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderApplication {
 
+    /**
+     * The main method to start the Order microservice application.
+     *
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
     }
