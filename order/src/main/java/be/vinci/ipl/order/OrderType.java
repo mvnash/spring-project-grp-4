f62055ac2ce422b6914026abf21fec3b58808f07
louisvanbel <louis.vanbel@student.vinci.be>
2023-11-16 14:07:43 +0100

    doc: add missing javadoc

--- order/src/main/java/be/vinci/ipl/order/OrderType.java
+++ order/src/main/java/be/vinci/ipl/order/OrderType.java
@@ -1,6 +1,17 @@
 package be.vinci.ipl.order;
 
+/**
+ * Enumeration representing the types of orders that are allowed within the VSX platform.
+ * Currently, it includes MARKET and LIMIT orders.
+ */
 public enum OrderType {
+  /**
+   * Represents a MARKET order, indicating an order to be executed immediately at the best available price.
+   */
   MARKET,
+
+  /**
+   * Represents a LIMIT order, indicating an order to be executed at a specified price (or better).
+   */
   LIMIT
 }
