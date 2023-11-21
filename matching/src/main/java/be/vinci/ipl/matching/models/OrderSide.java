9fb63ba5c9bee3a3dea3f206bd9e085cd8217541
louisvanbel <louis.vanbel@student.vinci.be>
2023-11-21 14:26:11 +0100

    feat: add matching models

---
+++ matching/src/main/java/be/vinci/ipl/matching/models/OrderSide.java
@@ -0,0 +1,16 @@
+package be.vinci.ipl.matching.models;
+
+/**
+ * Enumeration representing the side of an order, indicating whether it is a BUY or SELL order.
+ */
+public enum OrderSide {
+  /**
+   * Represents a BUY order, indicating an intention to purchase an instrument.
+   */
+  BUY,
+
+  /**
+   * Represents a SELL order, indicating an intention to sell an instrument.
+   */
+  SELL
+}
