2df8a0c3d67531dd1bc0867c26f34b3445d667fd
louisvanbel <louis.vanbel@student.vinci.be>
2023-11-21 17:42:44 +0100

    feat: add missing annotations

--- matching/src/main/java/be/vinci/ipl/matching/MatchingApplication.java
+++ matching/src/main/java/be/vinci/ipl/matching/MatchingApplication.java
@@ -2,10 +2,23 @@ package be.vinci.ipl.matching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * Main class for the Matching application.
+ * This class configures and launches the Spring Boot application.
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class MatchingApplication {
 
+    /**
+     * Main method to start the Spring Boot application.
+     *
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         SpringApplication.run(MatchingApplication.class, args);
     }
