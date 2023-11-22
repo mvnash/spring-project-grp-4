9fc7c196ba6c948b57b94e3a1c96cd8aee004bb7
Louis Vanbel <louis.vanbel@student.vinci.be>
2023-11-22 15:38:00 +0100

    feat: add missing annotation and dependency

--- order/src/main/java/be/vinci/ipl/order/models/OrderSide.java
+++ order/src/main/java/be/vinci/ipl/order/models/OrderSide.java
@@ -1,8 +1,12 @@
 package be.vinci.ipl.order.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
 /**
  * Enumeration representing the side of an order, indicating whether it is a BUY or SELL order.
  */
+@JsonFormat(shape = Shape.STRING)
 public enum OrderSide {
   /**
    * Represents a BUY order, indicating an intention to purchase an instrument.
