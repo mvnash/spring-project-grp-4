f6b44c32deee91a36801bd8c2599e92d0a048147
louisvanbel <louis.vanbel@student.vinci.be>
2023-11-22 14:01:52 +0100

    feat: add missing annotation

--- matching/src/main/java/be/vinci/ipl/matching/models/OrderType.java
+++ matching/src/main/java/be/vinci/ipl/matching/models/OrderType.java
@@ -1,9 +1,13 @@
 package be.vinci.ipl.matching.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
 /**
  * Enumeration representing the types of orders that are allowed within the VSX platform.
  * Currently, it includes MARKET and LIMIT orders.
  */
+@JsonFormat(shape = Shape.STRING)
 public enum OrderType {
   /**
    * Represents a MARKET order, indicating an order to be executed immediately at the best available price.
