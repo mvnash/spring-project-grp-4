ab45bce2f532115b18ca099b69fea44ff44485af
Navial <victor.denis@gmail.com>
2023-12-01 19:24:39 +0100

    gateway application: ressources

--- gateway/src/main/java/be/vinci/ipl/gateway/GatewayApplication.java
+++ gateway/src/main/java/be/vinci/ipl/gateway/GatewayApplication.java
@@ -2,12 +2,12 @@ package be.vinci.ipl.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class GatewayApplication {
 
   public static void main(String[] args) {
