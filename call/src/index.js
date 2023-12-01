77ee9cc78e2b0557c13e98dea72f3c6d15541e6b
Vlaminck Maxime <maxime.vlaminck@outlook.com>
2023-12-01 16:27:48 +0100

    fin script node js call

--- call/src/index.js
+++ call/src/index.js
@@ -1,72 +1,70 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
-const eureka = require('eureka-js-client');
+const eureka = require("eureka-js-client");
 
 const client = new eureka.Eureka({
-    instance: {
-        app: 'call',
-        hostName: 'localhost:9009',
-        ipAddr: '127.0.0.1',
-        statusPageUrl: 'http://localhost:9009/info',
-        port: {
-            '$': 9009,
-            '@enabled': true,
-        },
-        vipAddress: 'call',
-        dataCenterInfo: {
-            '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-            name: 'MyOwn',
-        }
+  instance: {
+    app: "call",
+    hostName: "localhost:9009",
+    ipAddr: "127.0.0.1",
+    statusPageUrl: "http://localhost:9009/info",
+    port: {
+      $: 9009,
+      "@enabled": true,
     },
-    eureka: {
-        host: 'localhost',
-        port: 9000,
-        servicePath: '/eureka/apps/',
-    }
+    vipAddress: "call",
+    dataCenterInfo: {
+      "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+      name: "MyOwn",
+    },
+  },
+  eureka: {
+    host: "localhost",
+    port: 9000,
+    servicePath: "/eureka/apps/",
+  },
 });
 
-client.start();
+client.start(async () => {
+  const investorsUrl = await getServiceUrl("investor");
+  const walletUrl = await getServiceUrl("wallet");
+
+  const responseInvestors = await fetch(`${investorsUrl}/investor`);
+  const investors = await responseInvestors.text();
+
+  for (investor in investors) {
+    const responseWallet = await fetch(
+      `${walletUrl}/wallet/${investor.username}`
+    );
+    const wallets = await responseWallet.text();
+
+    for (wallet in wallets) {
+      if (wallet.quantity < 0) {
+        cashToRemove = (wallet.unitvalue * Math.abs(wallet.quantity)) / 100;
+
+        await fetch(
+          `${walletUrl}/${investor.username}`,
+          {
+            method: "post",
+            body: {
+              ticker: "CASH",
+              quantity: cashToRemove,
+              unitvalue: 1,
+            },
+          }
+        );
+      }
+    }
+  }
+  
+  client.stop()
+});
 
 async function getServiceUrl(appName) {
-    const instances = await client.getInstancesByVipAddress(appName);
-    if (instances.length > 0) {
-        const instance = instances[0];
-        return `http://${instance.hostName}:${instance.port.$}/`;
-    }
-    throw new Error(`Aucune instance trouvée pour l'application ${appName}`);
+  const instances = await client.getInstancesByVipAddress(appName);
+  if (instances.length > 0) {
+    const instance = instances[0];
+    return `http://${instance.hostName}:${instance.port.$}/`;
+  }
+  throw new Error(`Aucune instance trouvée pour l'application ${appName}`);
 }
-
-const investorsUrl = await getServiceUrl('investor');
-const walletUrl = await getServiceUrl('wallet');
-
-async function verifyWallet() {
-
-    const responseInvestors = await fetch(`${investorsUrl}/investor`);
-    const investors = await responseInvestors.text();
-
-    const removed = false;
-    for (investor in investors) {
-
-        const responseWallet = await fetch(`${walletUrl}/wallet/${investor.username}`);
-        const wallet = await responseWallet.text();
-
-        if (wallet.quantity < 0) {
-            removed = true;
-            cashToRemove = (wallet.unitvalue * Math.abs(wallet.quantity)) / 100;
-
-
-            const responseWallet = await fetch(`${walletUrl}/${investor.username}`, {
-                method: 'post',
-                body: {
-                    "ticker": "CASH",
-                    "quantity": cashToRemove,
-                    "unitvalue": 1
-                  }
-            });
-
-        }
-    }
-    return removed;
-}
-
-verifyWallet()
