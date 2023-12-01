67a1fa761c72ac6b48450d1c2d4bc8aa3d75c94a
Vlaminck Maxime <maxime.vlaminck@outlook.com>
2023-12-01 19:53:07 +0100

    fix authentication

--- authentication/src/main/java/be/vinci/ipl/authentication/AuthenticationService.java
+++ authentication/src/main/java/be/vinci/ipl/authentication/AuthenticationService.java
@@ -11,7 +11,6 @@ import lombok.AllArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
-@AllArgsConstructor
 @Service
 public class AuthenticationService {
 
